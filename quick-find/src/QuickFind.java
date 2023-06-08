5910a47897f1101af4b9d4c5a62e8a742d3679e2
samwong59 <78820844+samwong59@users.noreply.github.com>
2023-06-08 15:26:18 +0100

    Create union method in QuickFind
    Creates a path between objects p and q. Sets id for any other paths created through transitive connections

--- quick-find/src/QuickFind.java
+++ quick-find/src/QuickFind.java
@@ -10,10 +10,21 @@ public class QuickFind {
         }
     }
 
+    // Checks for path between objects p and q
     public boolean connected(int p, int q) {
         return id[p] == id[q];
     }
 
+    // Creates path between objects p and q
+    public void union(int p, int q) {
+        int pId = id[p];
+        int qId = id[q];
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pId) {
+                id[i] = qId;
+            }
+        }
+    }
 
     public int[] getId() {
         return id;
