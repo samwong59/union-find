53089d6cf16de99ef1886bb6ab59a0449736d490
samwong59 <78820844+samwong59@users.noreply.github.com>
2023-06-10 18:49:25 +0100

    Fix problem set up
    Fixed problem where set up would skip union of p and q, and instead call union for the following path.

--- quick-find/src/Main.java
+++ quick-find/src/Main.java
@@ -8,16 +8,21 @@ public class Main {
         try {
             File problem = new File("tinyQF.txt");
             Scanner fileReader = new Scanner(problem);
-            QuickFind quickFind = new QuickFind(fileReader.nextInt());
+            int N = fileReader.nextInt();
+            QuickFind quickFind = new QuickFind(N);
             while (fileReader.hasNextInt()) {
                 int p = fileReader.nextInt();
                 int q = fileReader.nextInt();
                 if (!quickFind.connected(p, q)) {
-                    quickFind.union(fileReader.nextInt(), fileReader.nextInt());
-                    System.out.println(p + " " + "q" + " connected.");
+                    quickFind.union(p, q);
+                    System.out.println(p + " " + q + " connected.");
                 }
             }
-//
+//            int index = 0;
+//            for (int id : quickFind.getId()) {
+//                System.out.println(index + " " + id);
+//                index++;
+//            }
         } catch (IOException e) {
             e.printStackTrace();
         }
