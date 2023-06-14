a4f2d0bd17bfb5651975b64b589ca9a4a6eb69b2
samwong59 <78820844+samwong59@users.noreply.github.com>
2023-06-14 11:41:32 +0100

    Create QuickUnion constructor

---
+++ quick-union/src/QuickUnion.java
@@ -0,0 +1,11 @@
+public class QuickUnion {
+
+    private int[] id;
+
+    public QuickUnion(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+        }
+    }
+}
