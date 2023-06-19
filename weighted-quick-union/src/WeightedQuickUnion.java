ba06652a085bb2c5e92c633df21f7df62b14fb9f
samwong59 <78820844+samwong59@users.noreply.github.com>
2023-06-19 12:58:45 +0100

    Add constructor

--- weighted-quick-union/src/WeightedQuickUnion.java
+++ weighted-quick-union/src/WeightedQuickUnion.java
@@ -1,4 +1,15 @@
 public class WeightedQuickUnion {
 
+    private int[] id;
+    private int[] size;
+
+    public WeightedQuickUnion(int N) {
+        id = new int[N];
+        size = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            size[i] = 1;
+        }
+    }
     
 }
