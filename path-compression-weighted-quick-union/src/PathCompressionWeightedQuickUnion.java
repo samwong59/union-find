4520f6be7087ea6290184e1daddc0c142f812ae1
samwong59 <78820844+samwong59@users.noreply.github.com>
2023-07-22 12:15:23 +0100

    Create PCWQU logic

---
+++ path-compression-weighted-quick-union/src/PathCompressionWeightedQuickUnion.java
@@ -0,0 +1,38 @@
+public class PathCompressionWeightedQuickUnion {
+
+    private int[] id;
+    private int[] sz;
+
+    public PathCompressionWeightedQuickUnion(int N) {
+        id = new int[N];
+        sz = new int[N];
+        for (int i = 0; i < N; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+    }
+
+    private int root(int i) {
+        while (i != id[i]) {
+            id[i] = id[id[i]];
+            i = id[i];
+        }
+        return i;
+    }
+
+    private boolean find(int p, int q) {
+        return root(p) == root(q);
+    }
+
+    private void union(int p, int q) {
+        int i = root(p);
+        int j = root(q);
+        if (sz[i] < sz[j]) {
+            id[i] = j;
+            sz[j] += sz[i];
+        } else {
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+    }
+}
